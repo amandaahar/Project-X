e27d6507768c9497bb269b20b646cf555283366f
Aadhya Kocha <aadhya@fb.com>
2019-07-22 18:26:19 -0700

    Newly created event being sent to user with working location and date

--- ProjectX/ViewControllers/CreateEventViewController.m
+++ ProjectX/ViewControllers/CreateEventViewController.m
@@ -17,9 +17,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *createEventName;
 @property (weak, nonatomic) IBOutlet UITextField *createEventDescription;
 @property (weak, nonatomic) IBOutlet UITextField *createEventLocation;
-@property (weak, nonatomic) IBOutlet UITextField *createEventDate; //what about time
-@property (weak, nonatomic) IBOutlet UITextField *createAttendees;
-@property (weak, nonatomic) IBOutlet UIImageView *createPicture; //Do later
+@property (weak, nonatomic) IBOutlet UITextField *createEventDate;//Fix time
+@property (weak, nonatomic) IBOutlet UITextField *createAttendees;//Change to slider
+@property (weak, nonatomic) IBOutlet UIImageView *createPicture; //Finish later
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *createButton;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelButton;
 @property (nonatomic, readwrite) FIRFirestore *db;
@@ -27,11 +27,30 @@
 @end
 
 @implementation CreateEventViewController
+CLLocationCoordinate2D coordinate;
+UIDatePicker *datePicker;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.db = [FIRFirestore firestore];
     // Do any additional setup after loading the view.
+    datePicker = [[UIDatePicker alloc]init];
+    datePicker.datePickerMode = UIDatePickerModeDate;
+    [self.createEventDate setInputView:datePicker];
+    
+    UIToolbar *toolBar=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 44)];
+    [toolBar setTintColor:[UIColor grayColor]];
+    UIBarButtonItem *doneBtn=[[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStylePlain target:self action:@selector(ShowSelectedDate)];
+    UIBarButtonItem *space=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    [toolBar setItems:[NSArray arrayWithObjects:space,doneBtn, nil]];
+    [self.createEventDate setInputAccessoryView:toolBar];
+}
+
+- (void) ShowSelectedDate {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"dd/MMMM/YYYY hh:minmin a"];
+    self.createEventDate.text = [NSString stringWithFormat:@"%@", [formatter stringFromDate:datePicker.date]];
+    [self.createEventDate resignFirstResponder];
 }
 
 - (IBAction)OpenCameraButton:(id)sender {
@@ -55,46 +74,51 @@
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
-    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    //UIImage *editedImage = info[UIImagePickerControllerEditedImage];//Do I really need this
     
+    self.createPicture.image = [self resizeImage:originalImage withSize:CGSizeMake(400, 400)];
+                                
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 - (IBAction)didTapCreate:(id)sender {
-    /*
-    [[FirebaseManager sharedManager] getEvent:^(NSArray * _Nonnull event, NSError * _Nonnull error) {
-        if(error){
-            NSLog(@"Error creating event: %@", error.localizedDescription);
+
+    NSString *address = [NSString stringWithFormat:@"%@", self.createEventLocation.text];
+    [[[CLGeocoder alloc] init] geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error) {
+        if ([placemarks count] > 0) {
+            CLPlacemark *placemark = [placemarks objectAtIndex:0];
+            CLLocation *location = placemark.location;
+            coordinate = location.coordinate;
         }
-        else{
-            [self.delegate didCreate:event.firstObject];
-            NSLog(@"Creating event Success!");
-            [self dismissViewControllerAnimated:YES completion:nil];
-        }
+        FIRGeoPoint *geoPoint = [[FIRGeoPoint alloc] initWithLatitude:coordinate.latitude longitude:coordinate.longitude];
+        
+        NSNumberFormatter *Attendeesformatter = [[NSNumberFormatter alloc]init];
+        NSNumber *formattedNumOfAttendees = [Attendeesformatter numberFromString:self.createAttendees.text];
+        
+        __block FIRDocumentReference *ref = [[self.db collectionWithPath:@"Event"] addDocumentWithData:@{@"name": self.createEventName.text, @"description": self.createEventDescription.text, @"location": geoPoint, @"eventDate": [FIRTimestamp timestampWithDate: datePicker.date], @"numAttendees": formattedNumOfAttendees} completion:^(NSError * _Nullable error) {
+            if (error != nil) {
+                NSLog(@"Error adding document: %@", error);
+            } else {
+                NSLog(@"Document added with ID: %@", ref.documentID);
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }
+        }];
     }];
-     */
     
-    /*
-     [[[self.db collectionWithPath:@"Event"] documentWithPath:FIRAuth.auth.currentUser.uid] setData:@{
-           @"name": self.createEventName.text, @"description": self.createEventDescription.text, @"location": self.createEventLocation.text, @"date": self.createEventDate.text, @"numAttendees": self.createAttendees.text } completion:^(NSError * _Nullable error) {
-                if (error != nil) {
-                     NSLog(@"Error writing document: %@", error);
-                } else {
-                    NSLog(@"Document successfully written!");
-                    [self dismissViewControllerAnimated:YES completion:nil];
-                  }
-          }];
-     */
-    
-    __block FIRDocumentReference *ref = [[self.db collectionWithPath:@"Event"] addDocumentWithData:@{
-                                                                                                     @"name": @"Hey", @"description": @"HEY", @"location": [FIRGeoPoint new], @"date": [NSDate new], @"numAttendees": @10 } completion:^(NSError * _Nullable error) {
-                                                                                                         if (error != nil) {
-                                                                                                             NSLog(@"Error adding document: %@", error);
-                                                                                                         } else {
-                                                                                                             NSLog(@"Document added with ID: %@", ref.documentID);
-                                                                                                             [self dismissViewControllerAnimated:YES completion:nil];
-                                                                                                         }
-                                                                                                     }];
 }
                                                                                                      
 
