7c50ab0bde4ab868f40577fddff0fb81317e9fa9
Amanda Harris <amandahar@fb.com>
2019-07-24 18:02:34 -0700

    user can edit profile image and it pushed to storage in the database

--- ProjectX/ViewControllers/EditProfileViewController.m
+++ ProjectX/ViewControllers/EditProfileViewController.m
@@ -7,13 +7,15 @@
 //
 
 #import "EditProfileViewController.h"
+#import "../Models/FirebaseManager.h"
 
-@interface EditProfileViewController ()
+@interface EditProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIPickerView *interestsPicker;
 @property (weak, nonatomic) IBOutlet UIImageView *editedProfileImage;
 @property (weak, nonatomic) IBOutlet UITextField *firstNameText;
 @property (weak, nonatomic) IBOutlet UITextField *lastNameText;
 @property (weak, nonatomic) IBOutlet UITextField *bioText;
+@property (weak, nonatomic) IBOutlet UITextField *interests;
 
 @end
 
@@ -21,12 +23,92 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.tabBarController.tabBar setHidden: YES];
     // Do any additional setup after loading the view.
 }
 
 - (IBAction)didTapSave:(id)sender {
+    [self imageStorage];
     
 }
+
+- (IBAction)changeProfileImageButton:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    
+    else {
+        NSLog(@"Camera unavailable so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    //UIImage *editedImage = info[UIImagePickerControllerEditedImage];//Do I really need this
+    
+    self.editedProfileImage.image = [self resizeImage:originalImage withSize:CGSizeMake(400, 400)];
+    //    [self imageStorage];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (void) imageStorage {
+    
+    FIRStorage *storage = [FIRStorage storage];
+    NSUUID *randomID = [[NSUUID alloc] init];
+    FIRStorageReference *storageRef = [storage referenceWithPath:[@"profileImages/" stringByAppendingString:[NSString stringWithFormat: @"%@", randomID.description, @".jpg"]]];
+    //NSURL *localFile = [NSURL URLWithString:[NSString stringWithFormat:@"%@", storageRef]];
+    NSData *data = UIImageJPEGRepresentation(self.editedProfileImage.image, 0.75);
+    //FIRStorageReference *eventImagesRef = [storageRef child:@"images/mountains.jpg"];
+    FIRStorageMetadata *uploadMetaData = [[FIRStorageMetadata alloc] init];
+    uploadMetaData.contentType = @"image/jpeg";
+    
+    FIRStorageUploadTask *uploadTask = [storageRef putData:data metadata:uploadMetaData completion:^(FIRStorageMetadata *metadata, NSError *error) {
+        if (error != nil) {
+            NSLog(@"Uh-oh, first error occurred!");
+        } else {
+            // Metadata contains file metadata such as size, content-type, and download URL.
+            int size = metadata.size;
+            // You can also access to download URL after upload.
+            [storageRef downloadURLWithCompletion:^(NSURL * _Nullable URL, NSError * _Nullable error) {
+                if (error != nil) {
+                    NSLog(@"Uh-oh, second error occurred!");
+                } else {
+                    NSURL *downloadURL = URL;
+                    NSLog(@"Here is your downloaded URL: %@", downloadURL);
+                }
+            }];
+        }
+    }];
+    
+    [uploadTask observeStatus:FIRStorageTaskStatusSuccess handler:^(FIRStorageTaskSnapshot *snapshot) {
+        // Upload completed successfully
+        NSLog(@"Picture sending success!");
+    }];
+}
+
 
 
 /*
