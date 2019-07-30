1e23278c04691f2046a590f16dd263846d2c73fc
Alejandro Hernandez Lopez <alexhl09@fb.com>
2019-07-29 17:47:51 -0700

    Changed sign up view to show bubbles with preferences that the user likes

---
+++ ProjectX/Cells/PreferencesCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  PreferencesCollectionViewCell.h
+//  ProjectX
+//
+//  Created by alexhl09 on 7/29/19.
+//  Copyright © 2019 Charge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PreferencesCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
