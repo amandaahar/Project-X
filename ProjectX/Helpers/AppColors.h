5fe92c34c724167939234233f6e909c14335ceac
Alejandro Hernandez Lopez <alexhl09@fb.com>
2019-08-06 10:54:46 -0700

    Added colors

---
+++ ProjectX/Helpers/AppColors.h
@@ -0,0 +1,17 @@
+//
+//  AppColors.h
+//  ProjectX
+//
+//  Created by alexhl09 on 8/6/19.
+//  Copyright © 2019 Charge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppColors : NSObject
+@property (nonatomic, strong) UIColor *lightBlue;
+@end
+
+NS_ASSUME_NONNULL_END
