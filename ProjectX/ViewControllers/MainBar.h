d9fd34d67dbb8562e3b4808ab8975d2fa488a334
Alejandro Hernandez Lopez <alexhl09@fb.com>
2019-08-07 09:55:40 -0700

    Changes

---
+++ ProjectX/ViewControllers/MainBar.h
@@ -0,0 +1,17 @@
+//
+//  MainBar.h
+//  ProjectX
+//
+//  Created by alexhl09 on 8/6/19.
+//  Copyright © 2019 Charge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainBar : UINavigationBar
+
+@end
+
+NS_ASSUME_NONNULL_END
