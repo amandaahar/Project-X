e2620fc0e8753d3393c7f6cbeeb3d14ce1bc32f3
Alejandro Hernandez Lopez <alexhl09@fb.com>
2019-08-12 09:57:17 -0700

    New design

---
+++ ProjectX/LaunchScreen.m
@@ -0,0 +1,32 @@
+//
+//  LaunchScreen.m
+//  ProjectX
+//
+//  Created by alexhl09 on 8/10/19.
+//  Copyright © 2019 Charge. All rights reserved.
+//
+
+#import "LaunchScreen.h"
+
+@interface LaunchScreen ()
+
+@end
+
+@implementation LaunchScreen
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
